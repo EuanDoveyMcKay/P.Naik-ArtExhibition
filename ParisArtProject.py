c8d7fc3bf328f76a9f3dd6da12b2215d5cfa5640
Euan Dovey-McKay <euan.dovey-mckay@hotmail.com>
2024-02-18 08:53:14 +0000

    Added rasp pi  button functionality to code

--- ParisArtProject.py
+++ ParisArtProject.py
@@ -1,31 +1,19 @@
 import random
+from gpiozero import Button
+
+BtnInput = Button(2)
+# The integer passed into the function is the GPIO pin the button is connected to
 
 def ButtonPressed():
     ChanceInt = random.randint(1,1000)
     # Random number between 1 and 1000 to act as where in the range
 
     if ChanceInt <= 970:
-        return "chances say you have been sexually assaulted."
+        return True
     
     else:
-        return "chances say you have not been sexually assaulted."
+        return False
     
-    # If the random integer is in the bottom 970 values (lower 97%), it will return "you have" string. Else, will return "you haven't" string.
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-
-# This is the test loop if you wish to confirm that the chance of each reply is correct
-
-"""
-countN = 0
-countY = 0
-for i in range(0,100):
-    if ButtonPressed() == "chances say you have not been sexually assaulted.":
-        countN += 1
-
-    else:
-        countY += 1
-
-print(f"{countN} haven't, and {countY} have.")
-
-"""
+    # If the random integer is in the bottom 970 values (lower 97%), it will return False, otherwise it returns True
+    
+BtnInput.when_released = ButtonPressed
