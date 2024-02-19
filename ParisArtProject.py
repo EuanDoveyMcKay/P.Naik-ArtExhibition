cf8122431a2bc78d0ab417472d002624de13e410
Euan Dovey-McKay <euan.dovey-mckay@hotmail.com>
2024-02-19 17:24:31 +0000

    Un-commented the os.system(...) commands

--- ParisArtProject.py
+++ ParisArtProject.py
@@ -14,13 +14,11 @@ def InputPressed():
     # Random number between 1 and 1000 to act as where in the range
 
     if ChanceInt <= 970:
-        #os.system("lp SadFaceImage.png")
-        return True
+        os.system("lp SadFaceImage.png")
     
     else:
-        #os.system("lp SmileyFaceImage.png")
-        return False
-    # replace Booleans with the commented lines when code needs actual running
+        os.system("lp SmileyFaceImage.png")
+    # Prints the correct image for the result... MAKE SURE IMAGE FILES IN SAME DIR AS .PY!!!!!!
     
     # If the random integer is in the bottom 970 values (lower 97%), it will return False, otherwise it returns True
 
