6a44d8a3e6c3df5a199743e55aa12bbccbf77805
Euan Dovey-McKay <euan.dovey-mckay@hotmail.com>
2024-02-19 16:44:09 +0000

    Updated ParisArtProject.py code

--- ParisArtProject.py
+++ ParisArtProject.py
@@ -1,19 +1,42 @@
-import random
+"---------------------------------------------------------------------------------------------------------------------------------------"
+
+import random, os
 from gpiozero import Button
+from time import sleep
 
 BtnInput = Button(2)
 # The integer passed into the function is the GPIO pin the button is connected to
 
-def ButtonPressed():
+"---------------------------------------------------------------------------------------------------------------------------------------"
+
+def InputPressed():
     ChanceInt = random.randint(1,1000)
     # Random number between 1 and 1000 to act as where in the range
 
     if ChanceInt <= 970:
+        #os.system("lp %s" % SadFaceImage.png)
         return True
     
     else:
+        #os.system("lp %s" % SmileyFaceImage.png)
         return False
+    # replace Booleans with the commented lines when code needs actual running
     
     # If the random integer is in the bottom 970 values (lower 97%), it will return False, otherwise it returns True
-    
-BtnInput.when_released = ButtonPressed
+
+"---------------------------------------------------------------------------------------------------------------------------------------"
+
+BtnInput.when_released = InputPressed
+
+"---------------------------------------------------------------------------------------------------------------------------------------"
+
+try:
+    while True:
+        sleep(0.1)
+        # This is so the CPU doesn't overload when left idle
+
+except KeyboardInterrupt:
+    # KeyboardInterrupt is activated on the press of ctrl+C (sometimes ctrl+Z)
+    print("User has terminated program.")
+
+"---------------------------------------------------------------------------------------------------------------------------------------"
